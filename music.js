cfa481c0bd4903ec3aef823d595c9badb014a70c
rbwater3000 <bernilrodelit3000@gmail.com>
2025-10-24 04:56:57 +0800

    update

--- music.js
+++ music.js
@@ -9,3 +9,17 @@ menuToggle.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   sidebar.classList.remove('active');
 });
+
+// submenu toggle
+const toggleProducts = document.getElementById("toggle-products");
+const submenuProducts = document.getElementById("submenu-products");
+
+toggleProducts.addEventListener("click", () => {
+  if (submenuProducts.style.display === "none") {
+    submenuProducts.style.display = "block";
+    toggleProducts.textContent = "−";
+  } else {
+    submenuProducts.style.display = "none";
+    toggleProducts.textContent = "+";
+  }
+});
